7ef3312a5f1e4e5f02d2d31485b2af26c1fcba05
Eoin Gorman <eoingorman@live.ie>
2022-07-06 14:43:13 +0200

    migrate and seed db

--- db/migrate/20220706122702_create_tracks.rb
+++ db/migrate/20220706122702_create_tracks.rb
@@ -1,7 +1,9 @@
 class CreateTracks < ActiveRecord::Migration[7.0]
   def change
     create_table :tracks do |t|
-
+      t.string :name
+      t.string :minutes
+      t.references :album, foreign_key: true
       t.timestamps
     end
   end
